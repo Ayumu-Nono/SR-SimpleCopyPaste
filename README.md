19294aa0485775c9ebbe8e1c2edf8f3173644261
野村　湧 <y-nomura@nomurayuunoMacBook-Pro.local>
2021-04-08 20:51:50 +0900

    hello

--- README.md
+++ README.md
@@ -8,4 +8,22 @@
 4. 切り取り
 5. 貼り付け
 
+- マスクごと線形変換（平行移動・左右反転）をできるようにする。
+- 線形変換した後のマスクを背景画像に貼り付けられるようにする
+- 線形変換した後にアノテーションのポリゴンの位置が変わるはずなので、それも同じように移動されるようにする
+
 commit
+1. We randomly select two images and apply random scale jittering and random horizontal flipping on each of them. 
+    random scale jittering: 
+    
+    iaa.Affine(
+        scale={"x": (0.8, 1.2), "y": (0.8, 1.2)},
+        translate_px={"x": (-500, 500), "y": (-200, 200)})
+    random horizontal flipping:
+    iaa.Fliplr(0.5)
+    
+2. Then we select a random subset of objects from one of the images and paste them onto the other image.
+
+
+
+3. Lastly, we adjust the ground-truth annotations accordingly: we remove fully occluded objects and update the masks and bounding boxes of partially occluded objects.
--- read.py
+++ read.py
@@ -1,5 +1,8 @@
 from typing import List, Tuple
 from PIL import Image, ImageDraw
+import imgaug as ia
+from imgaug import augmenters as iaa
+import numpy as np
 
 im = Image.open('img/sample.jpg')
 im_bk = Image.open("img/background.jpg")
@@ -10,10 +13,26 @@ height: int = 256
 im = im.resize((width, height))
 im_bk = im_bk.resize((width, height))
 
+
 im_bk.save("img/background2.jpg")
+# 変換器の設定
+seq_aug = iaa.Sequential([
+    iaa.Fliplr(0.5),
+    iaa.Affine(
+        scale={"x": (0.8, 1.2), "y": (0.8, 1.2)},
+        translate_px={"x": (-100, 100), "y": (-100, 100)})
+])
+
+im_array = np.array(im)
+aug_imgs = seq_aug.augment_image(im_array)
+aug_jpg = Image.fromarray(aug_imgs)
+
+aug_jpg.save('img/augimg.jpg')
+
 mask = Image.new("L", im.size, 0)
 draw = ImageDraw.Draw(mask)
-polygons: List[Tuple[int, int]] = [(100, 100), (150, 40), (300, 200)]
+# polygons: List[Tuple[int, int]] = [(100, 100), (150, 40), (300, 200)]
+polygons: List[Tuple[int, int]] = [(100, 100), (150, 40), (300, 200), (100,300)]
 draw.polygon(polygons, fill=255)
 im_maked = Image.composite(im, im_bk, mask)
 im_maked.save('img/masked.jpg')
